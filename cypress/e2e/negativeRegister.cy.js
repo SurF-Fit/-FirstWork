2b12c740d3b92e0d31b59e57b8d1f08bd892cbe2
SurF-Fit <kkulmenev@gmail.com>
2025-03-03 14:05:26 +0700

    негативная регистрация готова

---
+++ cypress/e2e/negativeRegister.cy.js
@@ -0,0 +1,30 @@
+describe('Негативные тесты Регистрации', () => {
+    it('Попытка регистрации с уже существующим email', () => {
+        cy.visit('https://dev.profteam.su/registration');
+
+        cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type('Test');
+        cy.get('.form-input--email').type('testuser@example.com');
+        cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type('Password1');
+        cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type('Password1');
+
+        cy.get(':nth-child(4) > .button').click();
+
+    });
+
+    it('Попытка регистрации с пустыми полями', () => {
+        cy.visit('https://dev.profteam.su/registration');
+
+        cy.get(':nth-child(4) > .button').click();
+    });
+
+    it('Попытка регистрации с некорректным форматом email', () => {
+        cy.visit('https://dev.profteam.su/registration');
+
+        cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type('Test');
+        cy.get('.form-input--email').type('not-an-email');
+        cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type('Password1');
+        cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type('Password1');
+
+        cy.get(':nth-child(4) > .button').click();
+    });
+});
