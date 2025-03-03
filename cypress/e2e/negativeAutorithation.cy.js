193b9966adea97909c929cb84f2bd37f3349735c
SurF-Fit <kkulmenev@gmail.com>
2025-03-03 14:10:06 +0700

    Готова негативная авторизация

---
+++ cypress/e2e/negativeAutorithation.cy.js
@@ -0,0 +1,25 @@
+describe('Негативные тесты Авторизации', () => {
+    it('Попытка авторизации с неверным паролем', () => {
+        cy.visit('https://dev.profteam.su/login');
+
+        cy.get('.form-input--text').type('Test');
+        cy.get('.form-input--password').type('Password2');
+
+        cy.get(':nth-child(3) > .button').click();
+    });
+
+    it('Попытка авторизации с несуществующим email', () => {
+        cy.visit('https://dev.profteam.su/login');
+
+        cy.get('.form-input--text').type('Test123');
+        cy.get('.form-input--password').type('Password2');
+
+        cy.get(':nth-child(3) > .button').click();
+    });
+
+    it('Попытка авторизации с пустыми полями', () => {
+        cy.visit('https://dev.profteam.su/login');
+
+        cy.get(':nth-child(3) > .button').click();
+    });
+});
