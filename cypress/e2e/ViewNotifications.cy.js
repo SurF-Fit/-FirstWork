abdb62df7b631aeecf9a06fcb3bd3418ba9c782e
SurF-Fit <kkulmenev@gmail.com>
2025-03-03 15:49:14 +0700

    Расширил тесты с уведомлениями

---
+++ cypress/e2e/ViewNotifications.cy.js
@@ -0,0 +1,16 @@
+describe('Просмотр уведомлений', () => {
+    it('Просмотр уведомлений', () => {
+        cy.visit('https://dev.profteam.su/login');
+
+        cy.get('.form-input--text').type('Kulmenev');
+        cy.get('.form-input--password').type('Password1');
+
+        cy.get(':nth-child(3) > .button').click();
+
+        cy.get('[href="/notification"] > .header__label').click();
+
+        cy.get('.notification-title > .link').click()
+
+        cy.get(':nth-child(1) > .notification-list-item > .button').click()
+    });
+});
